fff2ea9c2d04c2e934c3a2d2e0a9ed5f9b7af398
Tomoya Sato <36657833+Tomoya-Sato@users.noreply.github.com>
2023-03-07 15:10:23 +0900

    Added PointXYZIRADT (#2)

--- include/map4_point_type/map4_point_type.h
+++ include/map4_point_type/map4_point_type.h
@@ -126,4 +126,27 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(PointOUSTER, (float, x, x)(float, y, y)(float,
                                                    std::uint8_t, ring, ring)(std::uint16_t, ambient,
                                                                              ambient)(std::uint32_t, range, range))
 
+struct PointXYZIRADT
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  uint16_t ring;
+  float azimuth;
+  float distance;
+  uint8_t return_type;
+  double time_stamp;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZIRADT,
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (float, intensity, intensity)
+                                  (std::uint16_t, ring, ring)
+                                  (float, azimuth, azimuth)
+                                  (float, distance, distance)
+                                  (std::uint8_t, return_type, return_type)
+                                  (double, time_stamp, time_stamp))
+
 #endif
